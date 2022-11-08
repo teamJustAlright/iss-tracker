3f289839c76e73a289da559e36f10ee7d5565aa3
mintedd <xxxxiheartyouxxxxx@gmail.com>
2022-11-07 23:13:54 -0500

    testing to see if this works with our pages

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# iss-tracker
+# project1
---
+++ google.js
@@ -0,0 +1,4 @@
+var apiKey = 'AIzaSyDJEWQJou3hsHRfdZJqEWnu8T6oKr1ewvc'
+var googleApiUrl = 'https://maps.googleapis.com/maps/api/js?key='
+var googleApi = googleApiUrl + apiKey
+console.log(googleApi)
